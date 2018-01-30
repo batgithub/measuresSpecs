c0259c76e970506a68504c2d19904532b0fe7f2a
baptiste <bath12@hotmail.fr>
2018-01-30 23:48:55 +0100

    - Ajout des components specs et sidebar dans la app.vue - suppression des elements default

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+    <aside class="sidebar">
+        <h1>Checkout</h1>
+    </aside>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+.sidebar {
+    width: 300px;
+    height: 100%;
+}
+</style>
---
+++ src/components/Specs.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+    <content class="iframe">
+        <iframe src="../static/checkout/index.html" width="100%" height="100%"></iframe>
+    </content>
+</template>
+
+<script>
+export default {
+    }
+</script>
+
+<style>
+.iframe {
+    height: 100%;
+    width: 100%;
+}
+</style>
