2125363cf8e38ddcd616bd633074220da55ae975
baptiste <lhopitault.baptiste@gmail.com>
2018-02-01 10:06:36 +0100

    - test import data via file (test.json)

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,24 @@
     <div class="specs-wrapper">
         <sidebar></sidebar>
         <specs></specs>
+        {{ md }}
     </div>
 </template>
 
 <script>
 import Specs from './components/Specs'
 import Sidebar from './components/Sidebar'
+import Test from '../static/Test.json'
 
 export default {
   components: {
     Specs,
     Sidebar
+  },
+  data(){
+    return{
+      md: Test
+    }
   }
 }
 </script>
