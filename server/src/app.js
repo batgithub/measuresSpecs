7196d6ecc6edaafa79afb5dcdd8cd96bdafcdfde
baptiste <bath12@hotmail.fr>
2018-02-10 17:27:36 +0100

    - uninstall package directory-tree - return child folder and return true if they contain file

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const fs = require('fs')
 
 //markdown parser
 var mds = require('markdown-serve'),
-    path = require('path');
+    path = require('path')
 
 
 const app = express()
@@ -14,43 +15,76 @@ app.use(bodyParser.json())
 app.use(cors())
 
 
-// folder parser
-const dirTree = require('directory-tree');
-const filteredTree = dirTree('../front/static/', {exclude:/.DS_Store/})
 
-app.get('/',(req, res) => {
-    res.send(
-      filteredTree
-    )
+
+
+
+
+//return children folder
+var dir = require('node-dir')
+
+
+
+var root = "../front/static/"
+var folders = fs.readdirSync(root)
+var objArray = [];
+folders.forEach((folder) => {
+    var obj    = {};
+    var files  = fs.readdirSync(root + folder);
+    var objArray2 = [];
+    var isSpec = function(){
+      var response = true
+      files.forEach((file) => {
+        var subfile = root + folder +'/' +file
+        if(fs.lstatSync(subfile).isDirectory()){}
+        else {
+          response = false
+        }
+      })
+      return response
+    }
+
+    obj.folderName = folder
+    obj.folderPath  = files
+    obj.isSpec = isSpec()
+
+    objArray.push(obj)
+})
+console.log(objArray)
+
+
+
+app.get('/p/:tagId', function(req, res) {
+  const Folder = '../front/static/' + req.params.tagId
+  console.log(Folder)
+  res.send(Folder)
+  // fs.readdir(Folder, (err, files) => {
+  //   files.forEach(file => {
+  //     res.send(file)
+  //     console.log(file)
+  //   })
+  // })
+
 })
 
 
-const fs = require('fs');
 
-//return children folder
 
-app.get('/p/:tagId', function(req, res) {
-  const Folder = '../front/static/' + req.params.tagId;
-  fs.readdir(Folder, (err, files) => {
-    files.forEach(file => {
-      res.send(file);
-      console.log(file);
-    });
-  })
 
-});
+
+
 
 // Parser MD
 app.use('/markdown', mds.middleware({
     rootDirectory: path.resolve(__dirname, '../../front/static/'),
-}));
+}))
 
 app.use(mds.middleware({
     rootDirectory: path.resolve(__dirname, '../../front/static/'),
     view: 'markdown'
-}));
+}))
 
 
 // Server listener
 app.listen(process.env.PORT || 8081)
-console.log("go on http://localhost:8081");
+console.log("go on http://localhost:8081")
