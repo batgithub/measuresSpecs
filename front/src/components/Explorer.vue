b2b2b105eb8e4525643ef14b7791d3d462b35f50
baptiste <lhopitault.baptiste@gmail.com>
2018-02-19 14:07:08 +0100

    style explorer vue, import font, icons

--- front/src/components/Explorer.vue
+++ front/src/components/Explorer.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="tree">
+  <div class="explorer">
     <div v-if="tree.iAmASpec === true">
       <measures :src="'../../../static'+urlPath.slice(2)+tree.theFolder+'/index.html'"></measures>
     </div>
     <div v-else>
-      <div class="" v-for="folder in tree.childFoldersArray">
-          <li><a :href="urlPath+folder.folderName+'/'">{{ folder.folderName }}</a></li>
-        {{error.message}}
+      <div class="container">
+        <h1>Measures</h1>
+        <div class="folders">
+          <a class="folder-wrapper" v-for="folder in tree.childFoldersArray" :href="urlPath+folder.folderName+'/'">
+            <div class="folder">
+                {{ folder.folderName }}
+            </div>
+
+            {{error.message}}
+          </a>
+
+        </div>
       </div>
     </div>
   </div>
@@ -45,8 +54,45 @@ export default {
   }
 }
 </script>
-<style>
-  li{
-    margin-bottom: 24px;
+
+
+<style lang="scss">
+
+@import './general.scss';
+.container h1{
+  padding: 0em 0.5rem;
+}
+.folders{
+  display: flex;
+  justify-content:space-between;
+  flex-wrap:wrap;
+}
+
+a.folder-wrapper {
+  box-sizing: border-box;
+  padding: 1em 1em;
+  margin: 0.5em;
+  width: calc(50% - 1em);
+  color:$gray-500;
+  text-decoration: none;
+  background: white;
+  border: 1px solid $gray-100;
+  border-radius: 5px;
+  align-items: center;
+
+  font-size: 1.3em;
+
+
+  .folder {
+
+    &::before {
+
+      content: url('../assets/folder-icon.svg');
+      display: inline-block;
+      vertical-align: middle;
+      margin-right: .5em;
+    }
+
   }
+}
 </style>
