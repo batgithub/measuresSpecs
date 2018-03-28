219233a5f6428ad4b9eb8d6ee502beb6d3683fcf
Baptiste Lhopitault <baptisel@pictarine.com>
2018-03-28 14:28:56 +0200

    Merge in one file server API and server launch app

--- server/src/app.js
+++ server/src/app.js
@@ -10,10 +10,13 @@ var mds = require('markdown-serve')
 var path = require('path')
 
 // start
-const app = express()
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
+const API = express()
+API.use(morgan('combined'))
+API.use(morgan('combined'))
+API.use(bodyParser.json())
+API.use(bodyParser.json())
+API.use(cors())
+API.use(cors())
 
 // return if we are in specs folder, his child folders and if they are specs folders
 var getChildFolders = function(folderPathRoot) {
@@ -76,7 +79,10 @@ var getChildFolders = function(folderPathRoot) {
 
 var rootFolder = '../front/static/explorerFiles'
 
-app.get('/measures/*', function(req, res) {
+
+
+API.get('/measures/*', function(req, res) {
+
   var checkIfParam = function(){
     if (req.params[0]){
       return req.params[0]+'/'
@@ -84,12 +90,13 @@ app.get('/measures/*', function(req, res) {
   }()
   const pathFolder = '../front/static/explorerFiles/' + req.params[0]
   res.setHeader('Content-Type', 'application/json');
+
   res.send(JSON.stringify(getChildFolders(pathFolder)))
 })
 
 
 // Parser MD
-app.use('/markdown', mds.middleware({
+API.use('/markdown', mds.middleware({
     rootDirectory: path.resolve(__dirname, '../../front/static/explorerFiles/'),
 }))
 
@@ -99,5 +106,18 @@ app.use('/markdown', mds.middleware({
 
 
 // Server listener
-app.listen(process.env.PORT || 8081)
-console.log("go on http://localhost:8081")
+API.listen(process.env.PORT || 8081)
+console.log("API REST on http://localhost:8081")
+
+
+
+
+// Server render app Vue js
+var history = require('connect-history-api-fallback')
+var serveStatic = require('serve-static')
+const app = express()
+
+app.use(history());
+app.use(serveStatic(__dirname + "../../../front/dist/"))
+app.listen(process.env.PORT || 8080);
+console.log('vuejs on http://localhost:8080 ')
