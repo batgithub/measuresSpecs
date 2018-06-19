c70bd0ec631b9f4d6bd40c7cac57689f39c0bc2b
Baptiste Lhopitault <baptisel@pictarine.com>
2018-06-19 14:18:33 +0200

    Add popup vue

---
+++ front/src/components/general/popup.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div class="background">
+      <div class="container">
+          <h1 class="title">Oyoyoyo</h1>
+          <span class="subtitle">Nouvelle version dispo</span>
+          <div class="content">
+          </div>
+          <div class="actions">
+              <buttons :link='' label='' className=''></buttons>
+
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import buttons from './buttons'
+export default {
+    components: {
+        buttons
+    },
+    props : [title, subtitle, content, idTriger]
+
+
+
+}
+</script>
+
+<style lang="scss">
+</style>
