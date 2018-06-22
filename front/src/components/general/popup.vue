3289add0faca5a8ed7326aa144f4eb184bd8733c
Baptiste Lhopitault <baptisel@pictarine.com>
2018-06-22 13:57:47 +0200

    Style popup with transition

--- front/src/components/general/popup.vue
+++ front/src/components/general/popup.vue
@@ -1,30 +1,96 @@
 <template lang="html">
-  <div class="background">
-      <div class="container">
-          <h1 class="title">Oyoyoyo</h1>
-          <span class="subtitle">Nouvelle version dispo</span>
-          <div class="content">
-          </div>
-          <div class="actions">
-              <buttons :link='' label='' className=''></buttons>
+    <transition name="fade">
+        <div v-if="showModal" class="popup" :id="idTriger">
+            <div class="container">
+                <h1 class="title">{{ title }}</h1>
+                <span class="subtitle">{{ subtitle }}</span>
+                <div class="content">
+                    <div class="content-wrap">
+                        {{ content }}
+                    </div>
+                </div>
+                <div class="actions">
+                    <buttons
+                        link=''
+                        :label="buttonLabel"
+                        class='button primary'
+                        @click="showModal = false"
+                        >
+                    </buttons>
 
-          </div>
-      </div>
-  </div>
+                </div>
+            </div>
+        </div>
+    </transition>
 </template>
 
 <script>
 import buttons from './buttons'
+
 export default {
+    name: 'popup',
     components: {
         buttons
     },
-    props : [title, subtitle, content, idTriger]
+    data () {
+        return {
+            showModal: false
 
-
+        }
+  },
+  props: ['title','subtitle', 'content', 'idTriger', 'buttonLabel']
 
 }
 </script>
 
 <style lang="scss">
+
+    @import '../general.scss';
+
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+      opacity: 0;
+    }
+
+    .popup {
+        display: flex;
+        position: absolute;
+        justify-content: center;
+        align-items: center;
+        background: rgba($gray-800,0.98);
+        color: white;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 1000;
+        text-align: center;
+
+        .title {
+            margin-bottom: .25em;
+        }
+        .subtitle {
+            font-size: 1.2em;
+        }
+        .content {
+            margin: 4em 0 4em;
+            border: 1px solid white;
+            padding: 2em;
+            border-radius: $radius;
+            max-width: 560px;
+            text-align: left;
+
+            .content-wrap {
+                max-height: 10em;
+                overflow-y: scroll;
+            }
+
+        }
+        .actions {
+            display: flex;
+            justify-content: center;
+        }
+    }
 </style>
--- front/src/components/measures/MeasuresView.vue
+++ front/src/components/measures/MeasuresView.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="specs-wrapper">
+        <popup
+            title="Oyoyoyo 🙌"
+            subtitle="Nouvelle version dispo"
+            content="oziejfzo efjozeijfozijef eozijfzeo ifjoijz eofijz oefijzofeij zofje zoifj zofijz ofeijzeofijz fojzof zjofijz foeijzf eojzofij zoefijz ziefhziefh izeuhfizehufizhf iuzhifhz ihfuizeuh fizeuhfizhu ifhzifhzifh zfuhz iefuhzifeuh ziufhezi hefizuhef izuhefiz hfiuhz ifuhzifuhziefh zihf ziuhef izhf izhuefiu zhfeizuh fihzif hzifhuizhuf izhf ziuhf izfizhu fihzuefi zhfihz ifuhzeifu hzoziejfzo efjozeijfozijef eozijfzeo ifjoijz eofijz oefijzofeij zofje zoifj zofijz ofeijzeofijz fojzof zjofijz foeijzf eojzofij zoefijz ziefhziefh izeuhfizehufizhf iuzhifhz ihfuizeuh fizeuhfizhu ifhzifhzifh zfuhz iefuhzifeuh ziufhezi hefizuhef izuhefiz hfiuhz ifuhzifuhziefh zihf ziuhef izhf izhuefiu zhfeizuh fihzif hzifhuizhuf izhf ziuhf izfizhu fihzuefi zhfihz ifuhzeifu hzoziejfzo efjozeijfozijef eozijfzeo ifjoijz eofijz oefijzofeij zofje zoifj zofijz ofeijzeofijz fojzof zjofijz foeijzf eojzofij zoefijz ziefhziefh izeuhfizehufizhf iuzhifhz ihfuizeuh fizeuhfizhu ifhzifhzifh zfuhz iefuhzifeuh ziufhezi hefizuhef izuhefiz hfiuhz ifuhzifuhziefh zihf ziuhef izhf izhuefiu zhfeizuh fihzif hzifhuizhuf izhf ziuhf izfizhu fihzuefi zhfihz ifuhzeifu hzoziejfzo efjozeijfozijef eozijfzeo ifjoijz eofijz oefijzofeij zofje zoifj zofijz ofeijzeofijz fojzof zjofijz foeijzf eojzofij zoefijz ziefhziefh izeuhfizehufizhf iuzhifhz ihfuizeuh fizeuhfizhu ifhzifhzifh zfuhz iefuhzifeuh ziufhezi hefizuhef izuhefiz hfiuhz ifuhzifuhziefh zihf ziuhef izhf izhuefiu zhfeizuh fihzif hzifhuizhuf izhf ziuhf izfizhu fihzuefi zhfihz ifuhzeifu hzoziejfzo efjozeijfozijef eozijfzeo ifjoijz eofijz oefijzofeij zofje zoifj zofijz ofeijzeofijz fojzof zjofijz foeijzf eojzofij zoefijz ziefhziefh izeuhfizehufizhf iuzhifhz ihfuizeuh fizeuhfizhu ifhzifhzifh zfuhz iefuhzifeuh ziufhezi hefizuhef izuhefiz hfiuhz ifuhzifuhziefh zihf ziuhef izhf izhuefiu zhfeizuh fihzif hzifhuizhuf izhf ziuhf izfizhu fihzuefi zhfihz ifuhzeifu hzoziejfzo efjozeijfozijef eozijfzeo ifjoijz eofijz oefijzofeij zofje zoifj zofijz ofeijzeofijz fojzof zjofijz foeijzf eojzofij zoefijz ziefhziefh izeuhfizehufizhf iuzhifhz ihfuizeuh fizeuhfizhu ifhzifhzifh zfuhz iefuhzifeuh ziufhezi hefizuhef izuhefiz hfiuhz ifuhzifuhziefh zihf ziuhef izhf izhuefiu zhfeizuh fihzif hzifhuizhuf izhf ziuhf izfizhu fihzuefi zhfihz ifuhzeifu hz"
+            buttonLabel= "OK, refresh"
+        >
+        </popup>
         <sidebar></sidebar>
         <content class="iframe">
             <iframe :src="src" width="100%" height="100%"></iframe>
@@ -10,11 +17,13 @@
 <script>
 
 import Sidebar from './SidebarView'
+import popup from '../general/popup'
 
 export default {
   props: ['src'],
   components: {
-    Sidebar
+    Sidebar,
+    popup
   }
 }
 </script>
